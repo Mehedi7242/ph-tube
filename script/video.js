6dfd5b6423339eecaa46fcda764850135f8e0fff
mehedi <mehedi2hasan7@gmail.com>
2024-10-04 17:57:08 +0600

    added categories section

--- script/video.js
+++ script/video.js
@@ -1,1 +1,32 @@
 //1 - Fetch, load, and show categories on html
+//create load categories 
+const loadCategories = () =>{
+    console.log('load categories')
+
+    // fetch data 
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+        .then((res) =>res.json())
+        .then((data)=>displayCategories(data.categories))
+        .catch((error) => console.error(error));
+
+};
+
+
+// {category_id: '1001', category: 'Music'}
+
+//create display categories
+const displayCategories = (categories) =>{
+    const categoryContainer = document.getElementById('categories');
+
+    categories.forEach(item => {
+        console.log(item)
+        const button = document.createElement('button');
+        button.classList = 'btn';
+        button.innerText = item.category; 
+        categoryContainer.append(button)
+    });
+
+
+}
+
+loadCategories();
