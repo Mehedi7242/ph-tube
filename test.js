699613c49214e47bfc49907af78abca187bca31f
mehedi <mehedi2hasan7@gmail.com>
2024-10-04 20:39:17 +0600

    done video section box,dinamic video,title,views

---
+++ test.js
@@ -0,0 +1,10 @@
+const isVarified = false;
+// if (isVarified === true){
+//     console.log();
+
+// }
+// else{
+//     console.log(`${isVarified === true ?"user is varified" :"user ins not varified"}`)
+// }
+console.log(`${isVarified === true ?"user is varified" :"user is not varified"}`)
+ 
